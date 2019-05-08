897e7304c86ae59d72c4ed067fe502ecfd602556
Phillip Zhang <pzhang@lcs.on.ca>
2019-05-08 17:08:46 -0400

    added the score values and possible scoring outcomes for the game based on how well you do

--- platform-game/MyWorld.java
+++ platform-game/MyWorld.java
@@ -11,6 +11,7 @@ public class MyWorld extends World
     private GreenfootImage bubble[];
     private int frames;
     private static final int delay = 5;
+    private int score;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -19,6 +20,9 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 450, 1); 
+        score = 0;
+        showScore();
+
         
         //Begin with the first bubble image  which will  be the the 'bubble'
         //array of 30 values
@@ -31,26 +35,44 @@ public class MyWorld extends World
         //Set the frame counter
         frames = 0;
     }
-    
+
     public void act()
     {
         // Track frames / time in scenario
-       frames += 1;
-        
-       // Track animation stage (e.g first image, second image, etc)
-       int stage = frames / delay;
-       // Change the image of the actor to create an animation
-       if (stage < bubble.length)
-       {
-           // Only update image once when it's time for a new stage of
-           // the animation
-           if (frames % delay == 0)
-           {
-               this.setImage(Greenfoot.GreenfootImagebubble);
-           }
-       }
+        frames += 1;
+
+        // Track animation stage (e.g first image, second image, etc)
+        int stage = frames / delay;
+        // Change the image of the actor to create an animation
+
     }
+
+   public void addScore(int points)
+   {
+      score = score + points;
+      showScore(); 
+   }
     
-        
-    
+   private void showScore()
+   {
+       if (score < 20)
+       {
+          showText("Bad: " + score, 80, 25); 
+       }
+       
+       if (score < 50)
+       {
+         showText("Okay: " + score, 80, 25); 
+       }
+       
+       if (score < 70)
+       {
+         showText("Great: " + score, 80, 25); 
+       }
+       
+       if (score <= 100)
+       {
+         showText("Perfect: " + score, 80, 25); 
+       }
+   }
 }
