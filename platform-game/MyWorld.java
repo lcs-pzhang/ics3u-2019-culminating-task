e66a411f464874de73a3b94c9182102e9fef00c2
Phillip Zhang <pzhang@lcs.on.ca>
2019-05-08 16:56:14 -0400

    got the background to be the first bubble image

--- platform-game/MyWorld.java
+++ platform-game/MyWorld.java
@@ -8,7 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-
+    private GreenfootImage bubble[];
+    private int frames;
+    private static final int delay = 5;
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -16,6 +18,39 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(600, 450, 1); 
+        
+        //Begin with the first bubble image  which will  be the the 'bubble'
+        //array of 30 values
+        bubble = new GreenfootImage[30];
+        //loop through the images from the images folder
+        for (int i = 0; i < bubble.length; i+=1)
+        {
+            bubble[i] = new GreenfootImage("bubble-" + i + ".gif");
+        }
+        //Set the frame counter
+        frames = 0;
     }
+    
+    public void act()
+    {
+        // Track frames / time in scenario
+       frames += 1;
+        
+       // Track animation stage (e.g first image, second image, etc)
+       int stage = frames / delay;
+       // Change the image of the actor to create an animation
+       if (stage < bubble.length)
+       {
+           // Only update image once when it's time for a new stage of
+           // the animation
+           if (frames % delay == 0)
+           {
+               this.setImage(Greenfoot.GreenfootImagebubble);
+           }
+       }
+    }
+    
+        
+    
 }
