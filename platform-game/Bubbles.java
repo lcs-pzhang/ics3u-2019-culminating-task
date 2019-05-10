1cfb3c592dc69ffb6602842ed3c23f392efa6628
Phillip Zhang <pzhang@lcs.on.ca>
2019-05-10 15:33:46 -0400

    got the background fully animated

---
+++ platform-game/Bubbles.java
@@ -0,0 +1,67 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class bubbles here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Bubbles extends Actor
+{
+    private GreenfootImage bubble[];
+    private int frames;
+    private static final int delay = 5;
+
+    /**
+     * Constructor
+     */
+    Bubbles()
+    {
+        //Begin with the first bubble image  which will  be the the 'bubble'
+        //array of 30 values
+        bubble = new GreenfootImage[30];
+
+        //loop through the images from the images folder
+        for (int index = 0; index < bubble.length; index+=1)
+        {
+            bubble[index] = new GreenfootImage("bubble-" + index + ".gif");
+        }
+
+        //Set the frame counter
+        frames = 0; 
+    }
+
+    /**
+     * Act - do whatever the bubbles wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Track frames / time in scenario
+        frames += 1;
+        System.out.println("frames " + frames);
+
+        // Track animation stage (e.g first image, second image, etc)
+        int stage = frames / delay;
+        System.out.println("stage " + stage);
+
+        // Change the image of the actor to create an animation
+        if (stage < bubble.length)
+        {
+            // Only update image once when it's time for a new stage of
+            // the animation
+            if (frames % delay == 0)
+            {
+                this.setImage(bubble[stage]);
+            }
+
+        }
+        else
+        {
+            //reset frames
+            frames = 0;
+
+        }
+
+    }    
+}
--- platform-game/MyWorld.java
+++ platform-game/MyWorld.java
@@ -8,10 +8,9 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class MyWorld extends World
 {
-    private GreenfootImage bubble[];
-    private int frames;
-    private static final int delay = 5;
     private int score;
+    private int frames;
+    private GreenfootImage bubble[];
     /**
      * Constructor for objects of class MyWorld.
      * 
@@ -19,36 +18,23 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 450, 1); 
+        super(498, 372, 1); 
         score = 0;
         showScore();
 
-        //Begin with the first bubble image  which will  be the the 'bubble'
-        //array of 30 values
-        bubble = new GreenfootImage[30];
-        //loop through the images from the images folder
-        for (int index = 0; index < bubble.length; index+=1)
-        {
-            bubble[index] = new GreenfootImage("bubble-" + index + ".gif");
-        }
-        //Set the frame counter
-        frames = 0;
+        // Add a Bubble object
+        Bubbles background = new Bubbles();
+        addObject(background, getWidth() / 2, getHeight() / 2);
+      
     }
 
     public void act()
     {
-        // Track frames / time in scenario
-        frames += 1;
-
-        // Track animation stage (e.g first image, second image, etc)
-        int stage = frames / delay;
-        // Change the image of the actor to create an animation
-        
         //When to start the song
         if (frames == 10)
         {
-          GreenfootSound sound = new GreenfootSound("Gift.mp3");
-          sound.play();
+            GreenfootSound sound = new GreenfootSound("Gift.mp3");
+            sound.play();
         }
     }
 
