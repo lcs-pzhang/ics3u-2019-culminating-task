9ea03018244d23c9ead9822470ccc8d6e1f062b2
Phillip Zhang <pzhang@lcs.on.ca>
2019-05-23 10:06:27 -0400

    made some changes to the code and editing the images

--- platform-game/MyWorld.java
+++ platform-game/MyWorld.java
@@ -42,31 +42,31 @@ public class MyWorld extends World
             "left + right", "up", "up", "up", "left", "left", "left", "left", "down", 
             "down", "down", "right", "right", "right"};
 
-    private int[] delay = {"10", "14", "18", "22", "24", "25", "30", "33", "38",
-            "42", "47", "50", "55", "58", "63", "67", "70", "72", "75", "78", "81", "84",
-            "86", "89", "90", "93", "96", "98", "100", "102", "105", "105", "107", "110",
-            "112", "113", "115", "118", "122", "124", "127", "128", "129", "130", "133",
-            "135", "137", "139", "141", "143", "143", "148", "148", "150", "152", "154",
-            "155", "157", "159", "161", "163", "164", "166", "168", "170", "170", "173",
-            "175", "177", "179", "180", "181", "182", "184", "187", "191", "195", "199"};
+    private int[] delay = {3, 14, 18, 22, 24, 25, 30, 33, 38,
+            42, 47, 50, 55, 58, 63, 67, 70, 72, 75, 78, 81, 84,
+            86, 89, 90, 93, 96, 98, 100, 102, 105, 105, 107, 110,
+            112, 113, 115, 118, 122, 124, 127, 128, 129, 130, 133,
+            135, 137, 139, 141, 143, 143, 148, 148, 150, 152, 154,
+            155, 157, 159, 161, 163, 164, 166, 168, 170, 170, 173,
+            175, 177, 179, 180, 181, 182, 184, 187, 191, 195, 199, 200, 201};
 
-    private int[] xPosition = {"495", "491", "487", "483", "460", "460","100",
-            "104", "108", "112", "150", "150", "175", "179", "183", "187", "195", "195",
-            "200", "206", "210", "220", "225", "220", "225", "230", "235", "240", "245",
-            "250", "247", "244", "241", "239", "115", "120", "121", "125", "127", "100",
-            "95", "92", "87", "85", "79", "84", "88", "67", "69", "73", "75", "77", "300",
-            "305", "307", "309", "330", "333", "336", "339", "350", "355", "357", "359", 
-            "370", "371", "379", "385", "383", "392", "395", "397", "400", "420", "421", 
-            "417", "440", "441", "444", "450"};
+    private int[] xPosition = {495, 491, 487, 483, 460, 460,100,
+            104, 108, 112, 150, 150, 175, 179, 183, 187, 195, 195,
+            200, 206, 210, 220, 225, 220, 225, 230, 235, 240, 245,
+            250, 247, 244, 241, 239, 115, 120, 121, 125, 127, 100,
+            95, 92, 87, 85, 79, 84, 88, 67, 69, 73, 75, 77, 300,
+            305, 307, 309, 330, 333, 336, 339, 350, 355, 357, 359, 
+            370, 371, 379, 385, 383, 392, 395, 397, 400, 420, 421, 
+            417, 440, 441, 444, 450};
 
-    private int[] yPosition = {"350", "350", "350", "350", "330", "325", "150",
-            "140", "130", "160", "165", "170", "171", "173", "175", "177", "177", "185",
-            "190", "193", "193", "195", "200", "200", "205", "200", "200", "210", "214",
-            "217", "220", "221", "225", "227", "230", "230", "230", "235", "239", "241",
-            "243", "248", "250", "50", "52", "54", "55", "57", "59", "65", "67", "69",
-            "71", "73", "75", "77", "42", "44", "48", "80", "83", "85", "90", "91", "94",
-            "95", "27", "30", "35", "39", "43", "300", "305", "309", "303", "314", "317",
-            "325", "335", "345"};
+    private int[] yPosition = {350, 350, 350, 350, 330, 325, 150,
+            140, 130, 160, 165, 170, 171, 173, 175, 177, 177, 185,
+            190, 193, 193, 195, 200, 200, 205, 200, 200, 210, 214,
+            217, 220, 221, 225, 227, 230, 230, 230, 235, 239, 241,
+            243, 248, 250, 50, 52, 54, 55, 57, 59, 65, 67, 69,
+            71, 73, 75, 77, 42, 44, 48, 80, 83, 85, 90, 91, 94,
+            95, 27, 30, 35, 39, 43, 300, 305, 309, 303, 314, 317,
+            325, 335, 345};
 
     /**
      * Constructor for objects of class MyWorld.
@@ -87,14 +87,21 @@ public class MyWorld extends World
         // Add a Bubble object
         Bubbles background = new Bubbles();
         addObject(background, getWidth() / 2, getHeight() / 2);
+        
+        
+        // Check the array lengths
+        // System.out.println("realNotes length is " + realNotes.length);
+        // System.out.println("delay length is " + delay.length);
+        // System.out.println("xPosition length is " + xPosition.length);
 
     }
 
     public void act()
     {
         countTime();
-
+        checkForNotes();
     }
+    
 
     public void addScore(int points)
     {
@@ -155,17 +162,32 @@ public class MyWorld extends World
         showText("Time: " + time, 560, 10);
     }
 
-    private void CheckForNotes()
+    private void checkForNotes()
     {
-        if (frames / 60 == 0)
+        // Increment frames
+        frames += 1;
+        
+        // checking that for each second, if a full second has passed.
+        if (frames % 60 == 0)
         {
-            for (int i = 0; i < realNotes.length; i +=1)
+            int currentSecond = frames / 60;
+            showText("" + currentSecond, getWidth() - 100, 100);
+            
+            //for each second, we're looping over every element of the real notes array/
+            for (int currentNote = 0; currentNote < realNotes.length; currentNote +=1)
             {
-                if (frames / 60 == delay[i])
+           
+                // is it time to check a new object
+                if (currentSecond == delay[currentNote])
                 {
-                    //add all notes in order
-                    addObject(star, 495, 350);
-
+                    //add just that one note (whatever note corresponds to currentNote)
+                    if (realNotes[currentNote] == "★")
+                    {
+                        Star newStar = new Star();
+                        addObject(newStar, xPosition[currentNote], 0);
+                    }
+                    //else if 
+                    
                 }
 
             }
